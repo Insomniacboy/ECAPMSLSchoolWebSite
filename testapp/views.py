18a4bab231e343032e472a39b971bf69e57756c7
muratsat <muratsat@users.noreply.github.com>
2022-01-30 02:04:18 +0300

    added readme

--- testapp/views.py
+++
@@ -1,8 +0,0 @@
-import django
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def testfunction(request):
-    return HttpResponse('yea it works')
