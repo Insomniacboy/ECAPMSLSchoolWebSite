2fc09b9febc969cee83b371f649872e994155577
muratsat <muratsat@users.noreply.github.com>
2022-01-30 02:07:40 +0300

    removed unused url

--- api/urls.py
+++ api/urls.py
@@ -19,5 +19,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('polls/', include('polls.urls')),
-    path('testapp/', include('testapp.urls'))
 ]
